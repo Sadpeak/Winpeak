5ca97d88cd77cbeb067c2763c99bdbdcb7826fd4
sadpeak <my@sadpeak.me>
2020-08-18 21:29:36 +0300

    add command that show top10 player of chossen server

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const Discord = require('discord.js');
 const mysql = require('mysql');
 
-//SELECT name, value FROM lvl_base ORDER BY value DESC LIMIT 10
 
 //databases
 
@@ -16,7 +15,7 @@ client.once("ready", () => {
     })
 });
 
-console.log(process.env.DB_HOST);
+
 let publicPool = mysql.createPool({
   host     : process.env.DB_HOST_PUBLIC,
   user     : process.env.DB_USER_PUBLIC,
@@ -38,32 +37,50 @@ pools = {
 };
 
 
-publicPool.getConnection(function(err, connection) {
-  if (err) throw err; // not connected!
- 
-  // Use the connection
-  connection.query('SELECT name, value FROM lvl_base ORDER BY value DESC LIMIT 10', function (error, results, fields) {
-    // When done with the connection, release it.
-    console.log(results);
-    connection.release();
- 
-    // Handle error after the release.
-    if (error) throw error;
- 
-    // Don't use the connection here, it has been returned to the pool.
-  });
-});
-
-
-
 
 //discord bot
 client.on('message', message => {
 	console.log(message.author.username,':',message.content);
-	if (message.content === '$ping') {
-	// send back "Pong." to the channel the message was sent in
-	message.channel.send('Pong.');
-}
+
+
+    const prefix = "$";
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd === "top" && args[0] == 'public' || args[0] == 'arena') {
+        let pool = pools[args];
+
+        pool.getConnection(function(err, connection) {
+          if (err) throw err; 
+         
+          
+          connection.query('SELECT name, value FROM lvl_base ORDER BY value DESC LIMIT 10', function (error, results, fields) {
+            
+                const exampleEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Топ 10 сервера tut doljno bit nazvanie ego')
+                    .addField(`1: ${results[0].name}`,`${results[0].value} exp`, false)
+                    .addField(`2: ${results[1].name}`,`${results[1].value} exp`, false)
+                    .addField(`3: ${results[2].name}`,`${results[2].value} exp`, false)
+                    .addField(`4: ${results[3].name}`,`${results[3].value} exp`, false)
+                    .addField(`5: ${results[4].name}`,`${results[4].value} exp`, false)
+                    .addField(`6: ${results[5].name}`,`${results[5].value} exp`, false)
+                    .addField(`7: ${results[6].name}`,`${results[6].value} exp`, false)
+                    .addField(`8: ${results[7].name}`,`${results[7].value} exp`, false)
+                    .addField(`9: ${results[8].name}`,`${results[8].value} exp`, false)
+                    .addField(`10: ${results[9].name}`,`${results[9].value} exp`, false)
+                    .setTimestamp()
+                    .setFooter('made by sadpeak', 'https://i.imgur.com/wSTFkRM.png');
+
+                    message.channel.send(exampleEmbed);
+                
+          });
+        });
+    }
 
 });
 client.login(process.env.TOKEN);
