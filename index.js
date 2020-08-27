c715ef0043a5878fb5bd00682ff6aa12ee89f486
sadpeak <my@sadpeak.me>
2020-08-27 17:47:32 +0300

    add $cmd command a.k.a rcon via discord

--- index.js
+++ index.js
@@ -1,77 +1,99 @@
 
 const Discord = require('discord.js');
 require('dotenv').config()
-const {get_top} = require('./utils/database.js');
-const {get_online} = require('./utils/database.js');
-
+const { get_top } = require('./utils/database.js');
+const { get_online } = require('./utils/database.js');
+const { exec_cmd } = require('./utils/database.js');
 
 
 const client = new Discord.Client();
 
 client.once("ready", () => {
-    console.log(`My name is ${client.user.username}`);
-    client.user.setPresence({
-        status: "dnd",
-        activity: {name: 'with your life'}
-    })
+  console.log(`My name is ${client.user.username}`);
+  client.user.setPresence({
+    status: "dnd",
+    activity: { name: 'with your life' }
+  })
 });
 
 
 
 //discord bot
 client.on('message', message => {
-    console.log(message.author.username,':',message.content);
+  console.log(message.author.username, ':', message.content);
 
+  const prefix = "$";
 
-    const prefix = "$";
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if (cmd === "top") {
-      if (args[0] == 'public' || args[0] == 'arena' || args[0] == 'awp') {
-
-              get_top(args[0]).then(results => {
-                console.log(results);
-                const topEmbed = new Discord.MessageEmbed()
-                    .setColor('#33FFFF')
-                    .setTitle(`Топ 10 сервера ${args[0]}`)
-                    for (let i = 0; i < 10; i++ ) {
-                      topEmbed.addField(`1: ${results[0].name}: ${results[0].value} exp`,`KDR: ${parseFloat(results[i].kills/results[i].deaths).toFixed(2)}. accuracy: ${parseFloat((results[i].hits/results[i].shoots)*100).toFixed(2)}% online: ${parseFloat(results[i].playtime/60/60).toFixed(2)} hours`, false)
-                    }
-                    topEmbed.setTimestamp()
-                    topEmbed.setFooter('made by sadpeak', 'https://i.imgur.com/wSTFkRM.png');
-
-                    message.channel.send(topEmbed);
-                
-          
-
-    }).catch(e => console.error(e));
-  }} 
-    if (cmd === 'online'){
-      if(args[0] == 'public' || args[0] == 'arena' || args[0] == 'awp') {
-    get_online(args[0]).then(status => {
-      let str = Array.from(status.match(/\s".*(["])\s/g));
-      let steamid = Array.from(status.match(/STEAM_\d:\d:\d*/g));
-      let curOnline = status.match(/players : \d+/).toString().split('players : ').pop();
-
-      const onlineEmbed = new Discord.MessageEmbed()
-      .setColor('#33FFFF')
-      .setTitle(`онлайн на сервере ${args[0]} ${curOnline}/30`)
-      
-      for(let i = 0; i < curOnline; i++) {
-       onlineEmbed.addField(`**` + str[i].replace(/["]/g, ' ') +`**\n`, steamid[i])
-      }
-      onlineEmbed.addField(`Текущая карта на сервере:`, `${status.match(/map     :\s.*/)}`);
-      message.channel.send(onlineEmbed);
-    }).catch(e => console.error(e));
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  if (args.length > 3) {
+    for (let i = 3; i < args.length; i++) {
+      args[2] += ' ' + args[i];
+      delete args[i];
     }
   }
-   if (cmd == 'help'){
+  const cmd = args.shift().toLowerCase();
+  if (cmd === "top") {
+    if (args[0] == 'public' || args[0] == 'arena' || args[0] == 'awp') {
+
+      get_top(args[0]).then(results => {
+        console.log(results);
+        const topEmbed = new Discord.MessageEmbed()
+          .setColor('#33FFFF')
+          .setTitle(`Топ 10 сервера ${args[0]}`)
+        for (let i = 0; i < 10; i++) {
+          topEmbed.addField(`1: ${results[0].name}: ${results[0].value} exp`, `KDR: ${parseFloat(results[i].kills / results[i].deaths).toFixed(2)}. accuracy: ${parseFloat((results[i].hits / results[i].shoots) * 100).toFixed(2)}% online: ${parseFloat(results[i].playtime / 60 / 60).toFixed(2)} hours`, false)
+        }
+        topEmbed.setTimestamp()
+        topEmbed.setFooter('made by sadpeak', 'https://i.imgur.com/wSTFkRM.png');
+
+        message.channel.send(topEmbed);
+
+
+
+      }).catch(e => console.error(e));
+    }
+  }
+  if (cmd === 'online') {
+    if (args[0] == 'public' || args[0] == 'arena' || args[0] == 'awp') {
+      get_online(args[0]).then(status => {
+        let str = Array.from(status.match(/\s".*(["])\s/g));
+        let steamid = Array.from(status.match(/STEAM_\d:\d:\d*/g));
+        let curOnline = status.match(/players : \d+/).toString().split('players : ').pop();
+
+        const onlineEmbed = new Discord.MessageEmbed()
+          .setColor('#33FFFF')
+          .setTitle(`онлайн на сервере ${args[0]} ${curOnline}/30`)
+
+        for (let i = 0; i < curOnline; i++) {
+          onlineEmbed.addField(`**` + str[i].replace(/["]/g, ' ') + `**\n`, steamid[i])
+        }
+        onlineEmbed.addField(`Текущая карта на сервере:`, `${status.match(/map     :\s.*/)}`);
+        message.channel.send(onlineEmbed);
+      }).catch(e => console.error(e));
+    }
+  }
+
+
+  if (cmd === 'cmd') {
+    if (args[0] == 'public' || args[0] == 'arena' || args[0] == 'awp') {
+      if (message.author.id = '215418064412344321') {
+        exec_cmd(args[0], args[1]).then(result => {
+          message.channel.send(result);
+        }).catch(e => console.error(e));
+
+      }
+      else {
+        message.channel.send('You don\'t have permission for this command');
+      }
+
+    }
+  }
+  if (cmd == 'help') {
     message.channel.send("Используй $top [server] или $online [server]\nДоступные сервера на данный момент: public, arena, awp");
   }
-});
+})
 client.login(process.env.TOKEN);
--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-require('dotenv').config({path: '../.env'})
+require('dotenv').config({ path: '../.env' })
 let Rcon = require('srcds-rcon');
 
 process.env.IP_ARENA
@@ -12,51 +12,51 @@ process.env.IP_AWP
 process.env.RCON_AWP
 
 let publicPool = mysql.createPool({
-    host     : process.env.DB_HOST_PUBLIC,
-    user     : process.env.DB_USER_PUBLIC,
-    port     : process.env.DB_PORT_PUBLIC,
-    password : process.env.DB_PASS_PUBLIC,
-    database : process.env.DB_DATABASE_PUBLIC
-  });
-  let arenaPool = mysql.createPool({
-    host     : process.env.DB_HOST_ARENA,
-    user     : process.env.DB_USER_ARENA,
-    port     : process.env.DB_PORT_ARENA,
-    password : process.env.DB_PASS_ARENA,
-    database : process.env.DB_DATABASE_ARENA
-  });
-  let awpPool = mysql.createPool({
-    host     : process.env.DB_HOST_AWP,
-    user     : process.env.DB_USER_AWP,
-    port     : process.env.DB_PORT_AWP,
-    password : process.env.DB_PASS_AWP,
-    database : process.env.DB_DATABASE_AWP
-  });
-  
-  pools = {
-      public: publicPool,
-      arena: arenaPool,
-      awp: awpPool
-  };
+    host: process.env.DB_HOST_PUBLIC,
+    user: process.env.DB_USER_PUBLIC,
+    port: process.env.DB_PORT_PUBLIC,
+    password: process.env.DB_PASS_PUBLIC,
+    database: process.env.DB_DATABASE_PUBLIC
+});
+let arenaPool = mysql.createPool({
+    host: process.env.DB_HOST_ARENA,
+    user: process.env.DB_USER_ARENA,
+    port: process.env.DB_PORT_ARENA,
+    password: process.env.DB_PASS_ARENA,
+    database: process.env.DB_DATABASE_ARENA
+});
+let awpPool = mysql.createPool({
+    host: process.env.DB_HOST_AWP,
+    user: process.env.DB_USER_AWP,
+    port: process.env.DB_PORT_AWP,
+    password: process.env.DB_PASS_AWP,
+    database: process.env.DB_DATABASE_AWP
+});
 
-  publicP = {
-    IP: process.env.IP_PUBLIC ,
-    RCON: process.env.RCON_PUBLIC 
-  }
-  arenaP = {
-      IP: process.env.IP_ARENA,
-      RCON: process.env.RCON_ARENA
-  }
-  awpP = {
-      IP: process.env.IP_AWP,
-      RCON: process.env.RCON_ARENA
-  }
-  
-  onlinePools = {
+pools = {
+    public: publicPool,
+    arena: arenaPool,
+    awp: awpPool
+};
+
+publicP = {
+    IP: process.env.IP_PUBLIC,
+    RCON: process.env.RCON_PUBLIC
+}
+arenaP = {
+    IP: process.env.IP_ARENA,
+    RCON: process.env.RCON_ARENA
+}
+awpP = {
+    IP: process.env.IP_AWP,
+    RCON: process.env.RCON_ARENA
+}
+
+onlinePools = {
     public: publicP,
     arena: arenaP,
     awp: awpP
-  }
+}
 
 module.exports.get_top = function (server) {
     let pool = pools[server];
@@ -77,18 +77,37 @@ module.exports.get_online = function (serverArg) {
     let server = onlinePools[serverArg];
 
     return new Promise((resolve, reject) => {
-    let rcon = Rcon({
-        address: server.IP,
-        password: server.RCON
-    });
-    rcon.connect().then(() => {
-        return rcon.command('status').then(status => resolve(status))
-        
-    }).then(
-        () => rcon.disconnect()
-    ).catch(err => {
-        console.log('caught', err);
-        console.log(err.stack);
-    });
-})
+        let rcon = Rcon({
+            address: server.IP,
+            password: server.RCON
+        });
+        rcon.connect().then(() => {
+            return rcon.command('status').then(status => resolve(status))
+
+        }).then(
+            () => rcon.disconnect()
+        ).catch(err => {
+            console.log('caught', err);
+            console.log(err.stack);
+        });
+    })
+}
+module.exports.exec_cmd = function (serverArg, cmd) {
+    let server = onlinePools[serverArg];
+
+    return new Promise((resolve, reject) => {
+        let rcon = Rcon({
+            address: server.IP,
+            password: server.RCON
+        });
+        rcon.connect().then(() => {
+            return rcon.command(cmd).then(result => resolve(result))
+
+        }).then(
+            () => rcon.disconnect()
+        ).catch(err => {
+            console.log('caught', err);
+            console.log(err.stack);
+        });
+    })
 }
